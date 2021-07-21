4b9799323bebded1bbc64a5d0dc951feec115a2e
Andy <a.kokovikhin@mail.ru>
2021-07-21 16:57:32 +0800

    5

--- ovner_info.py
+++ ovner_info.py
@@ -2,7 +2,7 @@ from bot_admin import BotAdmin
 from bot_user import BotUser
 from os import environ
 
-_ovner_id = environ['OVNER_ID'] 
+_ovner_id = int(environ['OVNER_ID']) 
 
 Ovner_Admin = BotAdmin(id=_ovner_id)
 Ovner_User = BotUser(id=_ovner_id)
